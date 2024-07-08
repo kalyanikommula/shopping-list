a490e64f8c2d8e3d1fa129100bc102448c863707
Kalyani Kommula <kalyanib.tech505@gmail.com>
2024-07-08 16:50:39 +0000

    install cloudinary

--- shopping_list/settings.py
+++ shopping_list/settings.py
@@ -17,6 +17,7 @@ if os.path.isfile('env.py'):
     import env
 
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -134,8 +135,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-CLOUDINARY_URL = os.getenv("CLOUDINARY_URL")
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
@@ -144,10 +143,10 @@ STATIC_URL = 'static/'
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR/'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#MEDIA_ROOT = BASE_DIR/'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
