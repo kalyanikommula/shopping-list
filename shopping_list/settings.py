b24adda831fddb4bc3782065bb6e81971511036d
Kalyani Kommula <kalyanib.tech505@gmail.com>
2024-07-09 11:09:00 +0000

    debug is false

--- shopping_list/settings.py
+++ shopping_list/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
  # SECRET_KEY = 'django-insecure-nj95gl@uly)lq&7$r%zcvn#&*8@y8$s98^^8&@cas=q1c!x*q5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-kalyanikomm-shoppinglis-mphp5jso91l.ws.codeinstitute-ide.net','.herokuapp.com']
 
