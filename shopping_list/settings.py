7da179ae1134e1ccb1692d9e6dcb2e42b1ffd462
Kalyani Kommula <kalyanib.tech505@gmail.com>
2024-07-12 12:24:45 +0000

    debug set to false

--- shopping_list/settings.py
+++ shopping_list/settings.py
@@ -156,8 +156,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'shop/static'), os.path.join(BASE_DIR,'media/product')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+#MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 #MEDIA_ROOT = BASE_DIR/'media'
+MEDIA_ROOT = 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
